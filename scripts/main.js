c650a69c42405a4a2ac3bac5dad20164164af434
Nhomar Hernandez <nhomar@gmail.com>
2013-08-03 02:58:54 -0700

    [DOING] Loading db list

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,44 @@
 $(document).ready(function(){
 });
 
+
+document.addEventListener('DOMContentLoaded', function (){
+    document.getElementById('signIn').addEventListener('click', connect)
+});
+document.addEventListener('DOMContentLoaded', function (){
+    document.getElementById('buttonDb').addEventListener('click', fillListDb)
+});
+
+function fillListDb(){
+    var server = document.getElementById('hostInputButton').value;
+    var request_db = new XmlRpcRequest(server+ '/xmlrpc/db');
+    console.log(request_db);
+}
+
+function connect(e){
+    /* TODO: Fill this values
+    var dbname = document.getElementById('database').value;
+    var user = document.getElementById('login').value;
+    var passwd = document.getElementById('password').value;
+    */
+    /*
+    var request = new XmlRpcRequest(server+ '/xmlrpc/common','login');
+    console.log(server);
+    request.addParam(dbname);
+    request.addParam(user);
+    request.addParam(passwd);
+    var response = request.send();
+    var uid = response.parseXML();
+    uid = parseInt(uid);
+    if (typeof uid === 'number' && !isNaN(uid)) {
+        chrome.storage.sync.set({'uid': uid, 'dbname':dbname,'user':user,'passwd':passwd,'server':server }, function(){
+        $('#oex_main').hide('slow');
+        $('#oex_timeline').show('slow');
+        });
+    } else {
+        alert('Incorrect Login, check your data again');
+    }
+    */
+    
+}
+
